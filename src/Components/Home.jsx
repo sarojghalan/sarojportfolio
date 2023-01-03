d4276a238384bc99aaa92456302cadd9cf14a821
Hypekage <jmestmnag@gmail.com>
2023-01-03 23:52:38 +0545

    inspiration banner created

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import InspirationBanner from './HomeComponents/InspirationBanner'
 import MasterBanner from './HomeComponents/MasterBanner'
+import MyServices from './HomeComponents/MyServices'
 
 const Home = () => {
   return (
     <>
         <MasterBanner />
+        <InspirationBanner />
+        <MyServices />
     </>
   )
 }
---
+++ src/Components/HomeComponents/InspirationBanner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner from '../../Assests/banner.png'
+
+const InspirationBanner = () => {
+  return (
+    <div style={{backgroundImage:`url(${banner})`}} className="inspiration-main">
+      <div className="container">
+        <h2 className='inspiration-title animate__animated  animate__bounceInDown'>Saroj G. : Be The Change <span><i class="fa-solid fa-cloud-sun-rain"></i></span> </h2>
+      </div>
+    </div>
+  )
+}
+
+export default InspirationBanner
